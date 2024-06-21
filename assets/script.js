68c4b02e25924045463eb158e55fd47fc96a71d5
neev0511 <allofbakwas@gmail.com>
2024-06-21 16:54:12 +0530

    Setting Password

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 const PATH = "https://calendar-backend-pc6v.onrender.com/getEvents/";
 // const PATH = "http://localhost:8000/getEvents/";
 
+localStorage.setItem("password", "r57854pK8yCdvF117s9VJqHhP4vXPN");
+
 const formatter = new Intl.DateTimeFormat("en-GB", {
   year: "numeric",
   month: "2-digit",
@@ -16,40 +18,43 @@ const formatter = new Intl.DateTimeFormat("en-GB", {
 const timeline = document.querySelector("#timeline");
 const loader = document.querySelector("#wrapper-loader");
 const getEvents = () => {
-  axios.post(PATH).then((allofRes) => {
-    loader.style.display = "none";
-    allofRes = allofRes.data;
-    count = 0;
-    for (let event of allofRes) {
-      alignment = "right";
-      if (count % 2 === 0) alignment = "left";
+  axios
+    .post(PATH, { password: localStorage.getItem("password") })
+    .then((allofRes) => {
+      loader.style.display = "none";
+      allofRes = allofRes.data;
+      count = 0;
+      for (let event of allofRes) {
+        alignment = "right";
+        if (count % 2 === 0) alignment = "left";
 
-      // Cleaning data
-      if (event.organizer.displayName === "Formula 1") {
-        image = "f1.png";
-        event.location = "https://www.fancode.com/";
-        event.description = event.summary;
-      } else if (event.organizer.displayName === "Boston University") {
-        image = "bu.png";
-        event.location = "https://www.google.com/maps?q=" + event.location;
-      } else if (event.organizer.displayName === "Airtribe Calendar")
-        image = "airtribe.svg";
-      else if (event.organizer.displayName === "Task") image = "tasks.svg";
-      else image = "other.jpg";
+        // Cleaning data
+        if (event.organizer.displayName === "Formula 1") {
+          image = "f1.png";
+          event.location = "https://www.fancode.com/";
+          event.description = event.summary;
+        } else if (event.organizer.displayName === "Boston University") {
+          image = "bu.png";
+          event.location = "https://www.google.com/maps?q=" + event.location;
+        } else if (event.organizer.displayName === "Airtribe Calendar")
+          image = "airtribe.svg";
+        else if (event.organizer.displayName === "Task") image = "tasks.svg";
+        else image = "other.jpg";
 
-      // Formatting dates
-      const startDate = new Date(event.start.dateTime);
-      const startFormattedDate = formatter.format(startDate);
+        // Formatting dates
+        const startDate = new Date(event.start.dateTime);
+        const startFormattedDate = formatter.format(startDate);
 
-      const endDate = new Date(event.end.dateTime);
-      const endFormattedDate = formatter.format(endDate);
+        const endDate = new Date(event.end.dateTime);
+        const endFormattedDate = formatter.format(endDate);
 
-      // Clearing undefined description
-      if (event.description === undefined) event.description = "No description";
+        // Clearing undefined description
+        if (event.description === undefined)
+          event.description = "No description";
 
-      // Making html code that needs to be added
+        // Making html code that needs to be added
 
-      content = `
+        content = `
               <div class="container ${alignment}-container">
         <img src="assets/images/${image}" alt="" />
         <div class="text-box">
@@ -62,22 +67,22 @@ const getEvents = () => {
           <span class="${alignment}-container-arrow"></span>
       `;
 
-      // Checking for location button
-      if (event.location !== undefined) {
-        content += `          
+        // Checking for location button
+        if (event.location !== undefined) {
+          content += `          
         <div class="button-wrapper">
             <button><a href="${event.location}">Open</a></button>
         </div>
           `;
+        }
+
+        content += `</div></div>`;
+
+        // Inputting content
+        timeline.innerHTML += content;
+        count += 1;
       }
-
-      content += `</div></div>`;
-
-      // Inputting content
-      timeline.innerHTML += content;
-      count += 1;
-    }
-  });
+    });
 };
 
 getEvents();
